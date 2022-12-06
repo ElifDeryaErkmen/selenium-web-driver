a03862a8aa16add5d6831484024affd13f48c109
ElifDeryaErkmen <75723000+ElifDeryaErkmen@users.noreply.github.com>
2022-12-06 19:58:07 +0300

    added the webd file
    none of this project works unless chromedriver is installed!!!!

---
+++ webd.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.keys import Keys
+import os
+from selenium import  webdriver
+
+os.environ['PATH'] = r"/home/derya1/Desktop/pythonProject/selenium/"
+driver = webdriver.Chrome()
